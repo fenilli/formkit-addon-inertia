a6528d2d3798eb6062d28970ddc05c7180eb2dfa
Gustavo Fenilli <gustavofenilli@gmail.com>
2022-10-22 21:26:04 -0300

    feat(package): :sparkles: adds formkit plugin back

--- server/resources/js/app.ts
+++ server/resources/js/app.ts
@@ -1,13 +1,19 @@
 import '../css/app.css'
+import '@formkit/themes/genesis'
 
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import { plugin as formkitPlugin, defaultConfig } from '@formkit/vue'
 
 createInertiaApp({
   resolve: (name) => require(`./Pages/${name}`),
   setup({ el, app, props, plugin }) {
     createApp({ render: () => h(app, props) })
       .use(plugin)
+      .use(formkitPlugin, defaultConfig())
       .mount(el)
   },
 })
+
+InertiaProgress.init()
